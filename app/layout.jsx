b71f7704c34e944fbf71db38850df5df1870655a
dalienst <oduordalienst@gmail.com>
2024-06-08 14:42:10 +0300

    add landing page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,16 @@
+"use client";
+
 import theme from "@/theme";
 import Provider from "./Provider";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>Entreprise Suite</title>
+        <title>Enterprise Suite</title>
         <meta name="description" content="Manage your business with ease." />
       </head>
       <body>
